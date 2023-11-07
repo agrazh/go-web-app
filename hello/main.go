f57ed45888e02728cc05100ce31b192f1b09a53b
agrazh <andy.grazh@gmail.com>
2023-11-07 21:15:06 +0100

    maps and slices

--- hello/main.go
+++ hello/main.go
@@ -1,26 +1,50 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
-type myStruct struct {
+type User struct {
 	FirstName string
+	LastName  string
 }
-
-// func printFirstName() string {}
-// `(m *myStruct)` - is a receiver. It ties function to `myStruct`. 
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
-}
-
 
 func main() {
-	var myVar myStruct
-	myVar.FirstName = "John"
+	// var myMap map[string]string
+	// `[string]` - is the index of the map, `int` - value stored
+	myMap := make(map[string]int)
 
-	myVar2 := myStruct{
-		FirstName: "Mary",
+	myMap["First"] = 1
+	myMap["Second"] = 2
+
+	log.Println(myMap["First"])
+	log.Println(myMap["Second"])
+	// maps are imutable
+	// `myMap := make(map[string]string)`
+	// - will throw an exception "no new variables on left side of :="
+
+	// Map of structs
+	users := make(map[string]User)
+
+	user1 := User{
+		FirstName: "Lucinda",
+		LastName:  "Tanner",
 	}
 
-	log.Println("myVar:", myVar.printFirstName())
-	log.Println("myVar2", myVar2.printFirstName())
+	users["user1"] = user1
+	log.Println(users["user1"].FirstName)
+
+	// Slice
+	var mySlice []int
+	numbers := []string{"one", "two", "three", "four"}
+
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
+
+	sort.Ints(mySlice)
+
+	log.Println(mySlice)
+	log.Println(numbers[0:2])
 }
--- info.md
+++ info.md
@@ -1,10 +1,72 @@
 ### Basics
 
 <details>
-<summary>Maps and Slices</summary>
+<summary>Decision structures</summary>
 
 ```
 CODE
+```
+</details>
+
+
+<details>
+<summary>Maps and Slices</summary>
+
+`myMap := make(map[string]int)` : `[string]` - is the index of the map, `int` - value stored at indexes.<br/>
+Map is mutable. Mutable types are passed by reference, imutable ones - by value.
+
+`numbers := []string` - slice of strings
+
+`sort` package
+
+```
+package main
+
+import (
+	"log"
+	"sort"
+)
+
+type User struct {
+	FirstName string
+	LastName  string
+}
+
+func main() {
+	// var myMap map[string]string
+	// `[string]` - is the index of the map, `int` - value stored
+	myMap := make(map[string]int)
+
+	myMap["First"] = 1
+	myMap["Second"] = 2
+
+	log.Println(myMap["First"])
+	log.Println(myMap["Second"])
+
+	// Map of structs
+	users := make(map[string]User)
+
+	user1 := User{
+		FirstName: "Lucinda",
+		LastName:  "Tanner",
+	}
+
+	users["user1"] = user1
+	log.Println(users["user1"].FirstName)
+
+	// Slice
+	var mySlice []int
+	numbers := []string{"one", "two", "three", "four"}
+
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
+
+	sort.Ints(mySlice)
+
+	log.Println(mySlice)
+	log.Println(numbers[0:2])
+}
 ```
 </details>
 
