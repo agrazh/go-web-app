38a7b7561cca930ee85f61e0f5cc5b12ae0598d5
agrazh <andy.grazh@gmail.com>
2023-11-12 22:27:44 +0100

    writing tests

--- info.md
+++ info.md
@@ -9,6 +9,77 @@ CODE
 </details>
 
 <details>
+<summary>Writing tests</summary>
+
+`$ go test`<br/>
+`$ go test -v`<br/>
+`$ go test -cover`<br/>
+`$ go test -coverprofile=coverage.out && go tool cover -html=coverage.out`</br>
+
+`main_test.go` :
+
+```
+package main
+
+import "testing"
+
+// //
+// Manual approach
+// //
+
+func TestDivide(t *testing.T) {
+	_, err := divide(10.0, 1.0)
+	if err != nil {
+		t.Error("Got an error when we should not have")
+	}
+}
+
+func TestBadDivide(t *testing.T) {
+	_, err := divide(10.0, 0)
+	if err == nil {
+		t.Error("Did not get an error when we should have")
+	}
+}
+
+// //
+// More efficient approch
+// //
+
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected  float32
+	isErr    bool
+}{
+	{"valid-data", 100.0, 5.0, 20.0, false},
+	{"invalid-data", 100.0, 0.0, 0.0, true},
+	{"expect-5", 50.0, 10.0, 5.0, false},
+}
+
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		got, err := divide(tt.dividend, tt.divisor)
+		
+		if tt.isErr {
+			if err == nil {
+				t.Error("expected an error but did not get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("did not expect an error but got one:", err.Error())
+			}
+		}
+
+		if got != tt.expected {
+			t.Errorf("expected %f but got %f", tt.expected, got)
+		}
+	}
+}
+```
+</details>
+
+<details>
 <summary>Reading and writing JSON</summary>
 
 `json.Unmarshal([]byte(myJson), &unmarshalled)` - deserialize<br/>
--- main.go
+++ main.go
@@ -1,68 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
+	"log"
 )
 
-type Person struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	HairColor string `json:"hair_color"`
-	HasDog    bool   `json:"has_dog"`
+func main() {
+	result, err := divide(100.0, 0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println(result)
 }
 
-func main() {
-	myJson := `
-[
-    {
-        "first_name": "Clark",
-        "last_name": "Kent",
-        "hair_color": "black",
-        "has_dog": true
-    },
-    {
-        "first_name": "Bruce",
-        "last_name": "Wayne",
-        "hair_color": "black",
-        "has_dog": false
-    }
-]`
+func divide(x, y float32) (float32, error) {
+	var result float32
 
-	// JSON -> struct
-	var unmarshalled []Person
-
-	err := json.Unmarshal([]byte(myJson), &unmarshalled)
-
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON", err)
+	if y == 0 {
+		return result, errors.New("division by 0")
 	}
 
-	fmt.Printf("unmarshalled: %v \n", unmarshalled)
-
-	// struct -> JSON
-	var mySlice []Person
-
-	m1 := Person{
-		FirstName: "Wally",
-		LastName:  "West",
-		HairColor: "red",
-		HasDog:    false,
-	}
-
-	m2 := Person{
-		FirstName: "Diane",
-		LastName:  "Prince",
-		HairColor: "black",
-		HasDog:    true,
-	}
-
-	mySlice = append(mySlice, m1, m2)
-
-	newJson, err := json.MarshalIndent(mySlice, "", "  ")
-	if err != nil {
-		fmt.Println("error marshaling:", err)
-	}
-
-	fmt.Println("marshaled: \n", string(newJson))
+	result = x / y
+	return result, nil
 }
